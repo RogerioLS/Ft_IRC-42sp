3c1dd232a8b129764fe99345ae928a30f784b484
Merovingio <60794624+RogerioLS@users.noreply.github.com>
2025-06-15 22:29:47 +0000

    feat(main): main test para make

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,7 +6,20 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 19:21:54 by codespace         #+#    #+#             */
-/*   Updated: 2025/06/07 19:21:55 by codespace        ###   ########.fr       */
+/*   Updated: 2025/06/15 22:05:27 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+// filepath: /workspaces/Ft_IRC-42sp/sources/main.cpp
+#include <iostream>
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        std::cerr << "Usage: ./ircserv <port> <password>" << std::endl;
+        return 1;
+    }
+    std::cout << "Server starting on port " << argv[1] << " with password " << argv[2] << std::endl;
+    return 0;
+}
