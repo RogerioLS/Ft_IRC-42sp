abf5023d44dd7f0770f16c048de59fdc3f8a0cb5
Merovingio <60794624+RogerioLS@users.noreply.github.com>
2025-07-14 00:45:00 +0000

    feat(command): Implement JOIN command
    - Adds the  function to the CommandHandler.
    - Logic handles both creating a new channel and joining an existing one.
    - The user who creates the channel is automatically set as an operator.

--- includes/command/CommandHandler.hpp
+++ includes/command/CommandHandler.hpp
@@ -1,34 +1,36 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   CommandHandler.hpp                                 :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ecoelho- <ecoelho-@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/07 19:21:31 by codespace         #+#    #+#             */
-/*   Updated: 2025/07/20 18:50:38 by ecoelho-         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef COMMANDHANDLER_HPP
 #define COMMANDHANDLER_HPP
 
-#include "../server/Client.hpp"
-#include "../server/Server.hpp"
-#include "../utils/Colors.hpp"
+#include "../utils/IRC.hpp"
+#include "../utils/Debug.hpp"
+#include <map>
+#include <string>
+#include <vector>
+
+class Server;
+class Client;
 
 class CommandHandler {
-	private:
-		CommandHandler();
-	public:
-		static void processCommand(Client &client, const std::string &command, class Server &server);
-		static void handlePass(Client &client, const std::vector<std::string> &args);
-		static void handleNick(Client &client, const std::vector<std::string> &args, class Server &server);
-		static void handleUser(Client &client, const std::vector<std::string> &args);
-		static void sendResponse(Client &client, const std::string &response);
-		static bool isValidNickname(const std::string &nickname);
-		static bool isNicknameInUse(const std::string &nickname, class Server &server);
+private:
+    typedef void (CommandHandler::*CommandFunction)(Client&, const std::vector<std::string>&);
+
+    Server& _server;
+    Debug&  _debug;
+    std::map<std::string, CommandFunction> _commands;
+
+    void _populateCommands();
+
+    // Command handlers
+    void _handleJoin(Client& client, const std::vector<std::string>& args);
+    void _handlePrivmsg(Client& client, const std::vector<std::string>& args);
+    void _handleTopic(Client& client, const std::vector<std::string>& args);
+    void _handleHelp(Client& client, const std::vector<std::string>& args);
+
+public:
+    CommandHandler(Server& server, Debug& debug);
+    ~CommandHandler();
+
+    void executeCommand(Client& client, const std::string& message);
 };
 
 #endif
-
