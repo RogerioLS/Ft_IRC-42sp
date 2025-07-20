b83ebae2e785fa7b095714104f6388d808aa56db
Eduardo Santos <educs98@outlook.com>
2025-07-20 17:33:39 -0300

    send

--- includes/server/Server.hpp
+++ includes/server/Server.hpp
@@ -6,7 +6,11 @@
 /*   By: ecoelho- <ecoelho-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 19:21:37 by codespace         #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2025/07/20 17:28:39 by ecoelho-         ###   ########.fr       */
+=======
+/*   Updated: 2025/07/04 13:00:42 by pmelo-ca         ###   ########.fr       */
+>>>>>>> cf65712 (fix: :bug: Update .gitignore to include build directory and ircserv; update timestamps in Server.hpp, Parser.cpp, and Server.cpp for consistency.)
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +27,13 @@ class Server {
 		std::string							_password;
 		int 										_serverFd;
 		int											_epollFd;
+<<<<<<< HEAD
 		volatile std::sig_atomic_t _gSignalStatus;
+=======
+		bool										_running;
+		int											_idCounter;
+		std::vector<Client>			_clientsVector;
+>>>>>>> cf65712 (fix: :bug: Update .gitignore to include build directory and ircserv; update timestamps in Server.hpp, Parser.cpp, and Server.cpp for consistency.)
 		std::vector<struct epoll_event> _eventsVector;
 		std::vector<Client>			_clientsVector;
 		std::vector<Channel>		_channelsVector;
@@ -55,11 +65,21 @@ class Server {
 		int getServerFd() const;
 		int getEpollFd() const;
 		int getClientCount() const;
+<<<<<<< HEAD
 		int getServerRunning() const;
 		// Setters
 		void setServerFd(int serverFd);
 		void setEpollFd(int epollFd);
 		void setServerRunning(int gSignalStatus);
+=======
+		bool getServerRunning() const;
+		int getServerIdCounter() const;
+		// Setters
+		void setServerFd(int serverFd);
+		void setEpollFd(int epollFd);
+		void setServerRunning(bool running);
+		void setServerIdCounter(int idCounter);
+>>>>>>> cf65712 (fix: :bug: Update .gitignore to include build directory and ircserv; update timestamps in Server.hpp, Parser.cpp, and Server.cpp for consistency.)
 
 		std::vector<Client>::iterator clientItFromFd(int fd);
 		template<typename T>
--- sources/parser/Parser.cpp
+++ sources/parser/Parser.cpp
@@ -7,10 +7,14 @@
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 19:21:57 by codespace         #+#    #+#             */
 <<<<<<< HEAD
+<<<<<<< HEAD
 /*   Updated: 2025/07/20 17:30:04 by ecoelho-         ###   ########.fr       */
 =======
 /*   Updated: 2025/07/03 13:05:55 by pmelo-ca         ###   ########.fr       */
 >>>>>>> 1cbd87a (fix: :bug: Update Parser and Client classes; rename parseBuffer to appendParsedCommand, add appendLineCommand method, and adjust buffer handling in Server class.)
+=======
+/*   Updated: 2025/07/04 13:16:47 by pmelo-ca         ###   ########.fr       */
+>>>>>>> cf65712 (fix: :bug: Update .gitignore to include build directory and ircserv; update timestamps in Server.hpp, Parser.cpp, and Server.cpp for consistency.)
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +35,12 @@ void Parser::appendParsedCommand(Client & client) {
 	while (true) {
 		pos = buffer.find('\n');
 		if (pos != std::string::npos) {
+			std::cout << BOLD << "Incomming buffer: " << '"' << buffer << '"' << RESET;
 			std::string messageSplitedByLine = buffer.substr(0, pos + 1);
 			client.setClientBufferStr(buffer.erase(0, pos + 1));
+			std::cout << CYAN << "MSBL: " << '"' << messageSplitedByLine << '"' << RESET;
 			appendLineCommand(messageSplitedByLine, client);
-		}
-		else
+		} else
 			break;
 	}
 >>>>>>> 1cbd87a (fix: :bug: Update Parser and Client classes; rename parseBuffer to appendParsedCommand, add appendLineCommand method, and adjust buffer handling in Server class.)
@@ -50,6 +55,8 @@ void Parser::appendLineCommand(const std::string & messageSplitedByLine, Client 
 	if (!line.empty() && line[line.length() - 1] == '\r')
 		line = line.substr(0, line.length() - 1);
 
-	if (!line.empty())
+	if (!line.empty()){
 		client.appendParsedCommand(line);
+		std::cout << GREEN << "Parsed Line: " << '"' << line << '"' << RESET << std:: endl;
+	}
 }
