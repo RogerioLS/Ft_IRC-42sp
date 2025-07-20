441d5bb5b6734bffc1a273c9bd9c53e87a13499b
Eduardo Santos <educs98@outlook.com>
2025-07-20 18:09:57 -0300

    send

--- sources/parser/Parser.cpp
+++ sources/parser/Parser.cpp
@@ -6,7 +6,7 @@
 /*   By: ecoelho- <ecoelho-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 19:21:57 by codespace         #+#    #+#             */
-/*   Updated: 2025/07/20 18:09:37 by ecoelho-         ###   ########.fr       */
+/*   Updated: 2025/07/20 18:09:51 by ecoelho-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,19 @@
 Parser::Parser() {}
 
 void Parser::parseBuffer(Client & client) {
-	
+	const std::string &buffer = client.getClientBuffer();
+
+	std::cout << "Raw buffer: ";
+
+	for (std::size_t i = 0; i < buffer.size(); ++i) {
+		unsigned char c = buffer[i];
+		if (c == '\r')
+			std::cout << "\\r";
+		else if (c == '\n')
+			std::cout << "\\n";
+		else
+			std::cout << c;
+}
+
+	std::cout << std::endl;
 }
