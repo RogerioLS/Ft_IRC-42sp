623760ca934994c4fcb334cdd5f0145cab0b7227
Eduardo Santos <educs98@outlook.com>
2025-07-20 18:09:45 -0300

    send

--- sources/parser/Parser.cpp
+++ sources/parser/Parser.cpp
@@ -6,7 +6,7 @@
 /*   By: ecoelho- <ecoelho-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 19:21:57 by codespace         #+#    #+#             */
-/*   Updated: 2025/07/20 18:09:14 by ecoelho-         ###   ########.fr       */
+/*   Updated: 2025/07/20 18:09:37 by ecoelho-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,47 +15,6 @@
 
 Parser::Parser() {}
 
-void Parser::appendParsedCommand(Client & client) {
-
-	std::string buffer = client.getClientBufferChar();
-	size_t pos ;
-
-	while (true) {
-		pos = buffer.find('\n');
-		if (pos != std::string::npos) {
-			std::cout << BOLD << "Incomming buffer: " << '"' << buffer << '"' << RESET;
-			std::string messageSplitedByLine = buffer.substr(0, pos + 1);
-			client.setClientBufferStr(buffer.erase(0, pos + 1));
-			std::cout << CYAN << "MSBL: " << '"' << messageSplitedByLine << '"' << RESET;
-			appendLineCommand(messageSplitedByLine, client);
-		} else
-			break;
-	}
-}
-
-void Parser::appendLineCommand(const std::string & messageSplitedByLine, Client & client) {
-	std::string line = messageSplitedByLine;
-
-	if (!line.empty() && line[line.length() - 1] == '\n')
-		line = line.substr(0, line.length() - 1);
-
-	if (!line.empty() && line[line.length() - 1] == '\r')
-		line = line.substr(0, line.length() - 1);
-
-	if (!line.empty()){
-		client.appendParsedCommand(line);
-		std::cout << GREEN << "Parsed Line: " << '"' << line << '"' << RESET << std:: endl;
-	}
-}
-
-std::vector<std::string> Parser::splitCommand(const std::string &command) {
-	std::vector<std::string> tokens;
-	std::istringstream iss(command);
-	std::string token;
-
-	while (iss >> token) {
-		tokens.push_back(token);
-	}
+void Parser::parseBuffer(Client & client) {
 	
-	return tokens;
 }
